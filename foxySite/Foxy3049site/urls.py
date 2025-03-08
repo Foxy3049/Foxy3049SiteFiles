dcbc5db22c79cbd814042e33f4dc986c670f53f2
Foxy3049 <96627700+Foxy3049@users.noreply.github.com>
2025-03-08 16:44:13 +0500

    Delete foxySite directory

--- foxySite/Foxy3049site/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('index.html', views.index, name="index"),
-    path('games.html', views.games, name="games")
-]
--- foxySite/Foxy3049site/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'index.html')
-
-def games(request):
-    return render(request, 'games.html')
