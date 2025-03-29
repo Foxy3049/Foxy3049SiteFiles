df4ac46baf73ab5928e1aaccd6939c6aea8ef98e
Foxy3049 <96627700+Foxy3049@users.noreply.github.com>
2025-03-29 16:30:37 +0500

    Delete foxySite directory

--- foxySite/Foxy3049site/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Foxy3049SiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Foxy3049site'
--- foxySite/Foxy3049site/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Register(models.Model):
-    email = models.CharField(max_length=200)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=50)
-    send_data = models.DateTimeField('Data send')
-    def __str__(self):
-        return f"{self.email} {self.username} {self.password}"
